b1e3a16a5dc18ad5b24ee100039bd92b4ee06b41
shukumar-542 <shukumar542@gmail.com>
2023-08-27 21:17:41 +0600

    solve ice creame parlor

---
+++ iceCremeParlor.js
@@ -0,0 +1,16 @@
+function icecreamParlor(m, arr) {
+    // Write your code here
+    
+    let result = [];
+    for(let i=0; i<arr.length;i++){
+       for(let j=i+1; j<arr.length;j++){
+           if(arr[i] + arr[j] === m){
+               result.push(i+1)
+               result.push(j+1)
+               return result;
+           }
+       }
+    }
+   
+
+}
