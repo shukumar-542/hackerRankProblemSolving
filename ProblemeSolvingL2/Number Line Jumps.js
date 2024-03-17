34c183f52325cff1d3df6d56675b06c8e1a2a473
shukumar-542 <shukumar542@gmail.com>
2024-03-17 11:15:54 +0600

    Solved Numer Line Jump

---
+++ ProblemeSolvingL2/Number Line Jumps.js
@@ -0,0 +1,20 @@
+let x1 =  21 
+let v1 = 6
+let x2 = 47
+let v2 = 3
+// const v2 = 2  
+function kangaroo(x1, v1, x2, v2) {
+    // Write your code here
+    
+    if (v1 <= v2) {
+        return "NO"
+    }
+    while(x1 <= x2){
+        x1 += v1
+        x2 += v2
+        if(x1 === x2){
+            return "YES"
+        }
+    }
+    return "NO"
+}
