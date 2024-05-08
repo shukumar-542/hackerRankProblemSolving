b160c6b6fa9163895ec4b88103d5cdd6a3169c85
shukumar-542 <shukumar542@gmail.com>
2024-05-08 21:58:54 +0600

    Solved cut the stick porblem

--- ProblemeSolvingL2/cutTheStick.js
+++ ProblemeSolvingL2/cutTheStick.js
@@ -1,1 +1,18 @@
-const arr = [ 5, 4, 4, 2, 2, 8 ]
+let  arr = [4, 5 ,10, 8 ,11]
+
+let result = [arr.length];
+
+
+
+
+while (arr.length > 0) {
+    const minValue = Math.min(...arr)
+    
+    arr = arr.filter(num => num != minValue).map(number => number - minValue)
+    if(arr.length !== 0){
+
+        result.push(arr.length)
+    }
+    
+}
+console.log(result);
