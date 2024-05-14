031a40c8b0a18cf9b9ca17d1e8b624077b4b3d4c
shukumar-542 <shukumar542@gmail.com>
2024-05-15 02:08:42 +0600

    clear callback function

--- callback.js
+++ callback.js
@@ -1,16 +1,12 @@
-const myNumbers = [4, 1, -20, -7, 5, 9, -6];
-
-const posNumber = (numbers )=>{
-    const newArr = []
-    for (const x of numbers) {
-       
-        if(x >= 0){
-            newArr.push(x)
-        }
-    }
-    return newArr
-}
-// const  removeNeg = (x)=> x >= 0;
-
-const result = posNumber(myNumbers);
-console.log(result);
+function fetchDataFromServer(callback) {
+    setTimeout(function() {
+      const data = { name: "John", age: 30 };
+      callback(data);
+    }, 2000);
+  }
+  
+  function processData(data) {
+    console.log("Data received:", data);
+  }
+  
+  fetchDataFromServer(processData);
