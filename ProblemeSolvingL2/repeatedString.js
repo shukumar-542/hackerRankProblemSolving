e5c90c4997f7f2a96aaeb62a13c1ab44e6fe3fc5
shukumar-542 <shukumar542@gmail.com>
2024-06-03 23:11:22 +0600

    find problem repeates string

--- ProblemeSolvingL2/repeatedString.js
+++ ProblemeSolvingL2/repeatedString.js
@@ -1,4 +1,34 @@
-const s = 'aba'
-const n = 10
+const s = 'gfcaaaecbg'
+const n = 547602
 
-console.log(a.length);
+let count = 0
+for(let i = 0 ; i<s.length; i++){
+    if(s[i] === 'a'){
+        count++
+    }
+}
+if(s.length === 1 && s ==='a'){
+    return n
+}else{
+    let result = ((n* count)/ s.length)
+    // return result
+    console.log(result);
+}
+
+function repeatedString(s, n) {
+    const divisionFloor = Math.floor(n / s.length);
+    const mod = n % s.length;
+    let count = 0;
+  
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] === "a") {
+        if (i < mod) {
+          count += 1;
+        }
+  
+        count += divisionFloor;
+      }
+    }
+  
+    return count;
+  }
